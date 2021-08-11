c318c85655b819e23e79de942944795d1ccbdf27
Alexander Peskov <peskovnn@gmail.com>
2021-08-11 17:55:23 +0300

    [MLPerf] initial runner
    Signed-off-by: Alexander Peskov <peskovnn@gmail.com>

---
+++ demo/mlperf_loadgen/classification.cpp
@@ -0,0 +1,163 @@
+#include "classification.h"
+
+
+Program::Program() {
+    settings = new CK::BenchmarkSettings();
+    session = new CK::BenchmarkSession(settings);
+
+    std::cout << "\nLoading graph..." << std::endl;
+    runtime = std::make_unique<RuntimeModule>(settings->graph_file());
+
+    tvm::ShapeTuple input_shape = settings->do_nchw_reorder
+            ? tvm::ShapeTuple({settings->batch_size, settings->num_channels, settings->image_size, settings->image_size})
+            : tvm::ShapeTuple({settings->batch_size, settings->image_size, settings->image_size, settings->num_channels});
+    tvm::ShapeTuple output_shape = {settings->batch_size, settings->num_classes};
+    DLDataType dtype{kDLFloat, 32, 1};
+    std::cout << CK::format("Input tensor dimensions: %d*%d*%d*%d", input_shape[0], input_shape[1], input_shape[2],
+                            input_shape[3]) << std::endl;
+    std::cout << CK::format("Output tensor dimensions: %d*%d", output_shape[0], output_shape[1]) << std::endl;
+
+    input_tensor = tvm::runtime::NDArray::Empty(input_shape, dtype, runtime->ctx);
+    output_tensor = tvm::runtime::NDArray::Empty(output_shape, dtype, runtime->ctx);
+    benchmark = std::make_unique<CK::Benchmark<float, CK::InNormalize, CK::OutCopy>>(settings,
+                                                                                     static_cast<float *>(input_tensor->data),
+                                                                                     static_cast<float *>(output_tensor->data));
+    benchmark->has_background_class = settings->num_classes == 1001;
+}
+
+Program::~Program() {
+    delete session;
+    delete settings;
+}
+
+void Program::LoadNextBatch(const std::vector<mlperf::QuerySampleIndex> &img_indices) {
+    auto vl = settings->verbosity_level;
+
+    if (vl > 1) {
+        std::cout << "LoadNextBatch([";
+        for (auto idx : img_indices) {
+            std::cout << idx << ' ';
+        }
+        std::cout << "])" << std::endl;
+    } else if (vl) {
+        std::cout << 'B' << std::flush;
+    }
+    session->load_filenames(img_indices);
+    benchmark->load_images(session);
+    if (vl) {
+        std::cout << std::endl;
+    }
+}
+
+void Program::ColdRun() {
+    auto vl = settings->verbosity_level;
+
+    if (vl > 1) {
+        std::cout << "Triggering a Cold Run..." << std::endl;
+    } else if (vl) {
+        std::cout << 'C' << std::flush;
+    }
+
+    module_inference();
+}
+
+int Program::InferenceOnce(int img_idx) {
+    benchmark->get_random_image(img_idx);
+    module_inference();
+    return benchmark->get_next_result();
+}
+
+void Program::UnloadBatch(const std::vector<mlperf::QuerySampleIndex> &img_indices) {
+    auto b_size = img_indices.size();
+
+    auto vl = settings->verbosity_level;
+
+    if (vl > 1) {
+        std::cout << "Unloading a batch[" << b_size << "]" << std::endl;
+    } else if (vl) {
+        std::cout << 'U' << std::flush;
+    }
+
+    benchmark->unload_images(b_size);
+}
+
+
+void Program::module_inference() {
+    runtime->set_input(0, input_tensor);
+    TVMSynchronize(runtime->ctx.device_type, runtime->ctx.device_id, nullptr);
+    runtime->run();
+    TVMSynchronize(runtime->ctx.device_type, runtime->ctx.device_id, nullptr);
+    tvm::runtime::NDArray tvm_output = runtime->get_output(0);
+    output_tensor.CopyFrom(tvm_output);
+    TVMSynchronize(runtime->ctx.device_type, runtime->ctx.device_id, nullptr);
+}
+
+
+SystemUnderTestTVM::SystemUnderTestTVM(Program *_prg) : mlperf::SystemUnderTest() {
+    prg = _prg;
+    query_counter = 0;
+};
+
+void SystemUnderTestTVM::IssueQuery(const std::vector<mlperf::QuerySample> &samples) {
+
+    ++query_counter;
+    auto vl = prg->settings->verbosity_level;
+    if (vl > 1) {
+        std::cout << query_counter << ") IssueQuery([" << samples.size() << "]," << samples[0].id << ","
+                  << samples[0].index << ")" << std::endl;
+    } else if (vl) {
+        std::cout << 'Q' << std::flush;
+    }
+
+    std::vector<mlperf::QuerySampleResponse> responses;
+    responses.reserve(samples.size());
+    float encoding_buffer[samples.size()];
+    int i = 0;
+    for (auto s : samples) {
+        int predicted_class = prg->InferenceOnce(s.index);
+
+        if (vl > 1) {
+            std::cout << "Query image index: " << s.index << " -> Predicted class: " << predicted_class << std::endl
+                      << std::endl;
+        } else if (vl) {
+            std::cout << 'p' << std::flush;
+        }
+
+        encoding_buffer[i] = (float) predicted_class;
+        responses.push_back({s.id, uintptr_t(&encoding_buffer[i]), sizeof(encoding_buffer[i])});
+        ++i;
+    }
+    mlperf::QuerySamplesComplete(responses.data(), responses.size());
+}
+
+void SystemUnderTestTVM::FlushQueries() {
+    auto vl = prg->settings->verbosity_level;
+    if (vl) {
+        std::cout << std::endl;
+    }
+}
+
+void SystemUnderTestTVM::ReportLatencyResults(const std::vector<mlperf::QuerySampleLatency>& latencies_ns) {
+    size_t size = latencies_ns.size();
+    uint64_t avg = accumulate(latencies_ns.begin(), latencies_ns.end(), uint64_t(0)) / size;
+
+    std::vector<mlperf::QuerySampleLatency> sorted_lat(latencies_ns.begin(), latencies_ns.end());
+    sort(sorted_lat.begin(), sorted_lat.end());
+
+    std::cout << std::endl << "------------------------------------------------------------";
+    std::cout << std::endl << "|            LATENCIES (in nanoseconds and fps)            |";
+    std::cout << std::endl << "------------------------------------------------------------";
+    size_t p50 = size * 0.5;
+    size_t p90 = size * 0.9;
+    std::cout << std::endl << "Number of queries run: " << size;
+    std::cout << std::endl << "Min latency:                      " << sorted_lat[0]       << "ns  (" << 1e9/sorted_lat[0]         << " fps)";
+    std::cout << std::endl << "Median latency:                   " << sorted_lat[p50]     << "ns  (" << 1e9/sorted_lat[p50]       << " fps)";
+    std::cout << std::endl << "Average latency:                  " << avg                 << "ns  (" << 1e9/avg                   << " fps)";
+    std::cout << std::endl << "90 percentile latency:            " << sorted_lat[p90]     << "ns  (" << 1e9/sorted_lat[p90]       << " fps)";
+
+    if(!prg->settings->trigger_cold_run) {
+        std::cout << std::endl << "First query (cold model) latency: " << latencies_ns[0]     << "ns  (" << 1e9/latencies_ns[0]       << " fps)";
+    }
+    std::cout << std::endl << "Max latency:                      " << sorted_lat[size-1]  << "ns  (" << 1e9/sorted_lat[size-1]    << " fps)";
+    std::cout << std::endl << "------------------------------------------------------------ " << std::endl;
+}
---
+++ demo/mlperf_loadgen/classification.h
@@ -0,0 +1,99 @@
+#ifndef TVM_LOADGEN_CLASSIFICATION_H
+#define TVM_LOADGEN_CLASSIFICATION_H
+
+#include <algorithm>
+#include <memory>
+#include <numeric>
+
+#include "tvm/runtime/module.h"
+#include "tvm/runtime/packed_func.h"
+
+#include "loadgen.h"
+#include "query_sample_library.h"
+#include "system_under_test.h"
+
+#include "test_settings.h"
+#include "benchmark.h"
+
+
+struct RuntimeModule {
+    DLDevice ctx{};
+    tvm::runtime::PackedFunc set_input;
+    tvm::runtime::PackedFunc get_output;
+    tvm::runtime::PackedFunc run;
+
+    explicit RuntimeModule(const std::string& module_path) {
+        ctx = {kDLCPU, 0};
+        tvm::runtime::Module mod_factory = tvm::runtime::Module::LoadFromFile(module_path);
+        tvm::runtime::Module gmod = mod_factory.GetFunction("default")(ctx);
+        set_input = gmod.GetFunction("set_input");
+        get_output = gmod.GetFunction("get_output");
+        run = gmod.GetFunction("run");
+    }
+};
+
+
+class Program {
+public:
+    Program();
+    ~Program();
+
+    void LoadNextBatch(const std::vector<mlperf::QuerySampleIndex>& img_indices);
+    void ColdRun();
+    int InferenceOnce(int img_idx);
+    void UnloadBatch(const std::vector<mlperf::QuerySampleIndex>& img_indices);
+
+    int available_images_max() const { return settings->list_of_available_imagefiles().size(); }
+    int images_in_memory_max() const { return settings->images_in_memory_max; }
+
+    CK::BenchmarkSettings *settings;
+private:
+    void module_inference();
+
+    CK::BenchmarkSession *session;
+    std::unique_ptr<CK::IBenchmark> benchmark;
+    std::unique_ptr<RuntimeModule> runtime;
+    tvm::runtime::NDArray input_tensor;
+    tvm::runtime::NDArray output_tensor;
+};
+
+
+class SystemUnderTestTVM : public mlperf::SystemUnderTest {
+public:
+    explicit SystemUnderTestTVM(Program *_prg);
+    ~SystemUnderTestTVM() override = default;
+
+    const std::string& Name() const override { return name_; }
+    void IssueQuery(const std::vector<mlperf::QuerySample>& samples) override;
+    void FlushQueries() override;
+    void ReportLatencyResults(const std::vector<mlperf::QuerySampleLatency>& latencies_ns) override;
+
+private:
+    std::string name_{"TVM_SUT"};
+    Program *prg;
+    long query_counter;
+};
+
+class QuerySampleLibraryTVM : public mlperf::QuerySampleLibrary {
+public:
+    explicit QuerySampleLibraryTVM(Program *_prg) : mlperf::QuerySampleLibrary() {
+        prg = _prg;
+    };
+    ~QuerySampleLibraryTVM() override = default;
+
+    const std::string& Name() const override { return name_; }
+    size_t TotalSampleCount() override { return prg->available_images_max(); }
+    size_t PerformanceSampleCount() override { return prg->images_in_memory_max(); }
+    void LoadSamplesToRam( const std::vector<mlperf::QuerySampleIndex>& samples) override {
+        prg->LoadNextBatch(samples);
+    }
+    void UnloadSamplesFromRam( const std::vector<mlperf::QuerySampleIndex>& samples) override {
+        prg->UnloadBatch(samples);
+    }
+
+private:
+    std::string name_{"TVM_QSL"};
+    Program *prg;
+};
+
+#endif //TVM_LOADGEN_CLASSIFICATION_H
